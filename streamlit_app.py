fa8f1ec648abfbd2a850aec079ad61c97604c5a2
dorposner <77918634+dorposner@users.noreply.github.com>
2025-06-04 16:00:11 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ from data_helpers import (
 )
 
 def main():
-    st.title("CYJ Hugim Allocation Web App Test")
+    st.title("CYJ Hugim Allocation Web App")
 
     with st.expander("📄 Click here for instructions (ignore column names if using your own):"):
         st.markdown("""
